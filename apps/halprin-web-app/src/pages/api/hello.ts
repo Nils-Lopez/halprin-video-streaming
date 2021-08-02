260dd2d694b3731b480eb1f2308734ad824ac3ec
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-02 09:52:39 +0200

    feat: initial life-art project

--- apps/halprin-web-app/src/pages/api/hello.ts
+++ apps/halprin-web-app/src/pages/api/hello.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { sayHello } from '@contredanse/common';
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-  return res.send(sayHello('world loaded from /api/hello'));
+  return res.send('hello');
 }
