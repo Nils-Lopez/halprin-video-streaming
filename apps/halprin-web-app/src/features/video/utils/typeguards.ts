77be054b8d6d4f5678e8062133b4248c30919448
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-08-10 22:36:44 +0200

    feat: typeguard for LocalizedVideoUrl

--- apps/halprin-web-app/src/features/video/utils/typeguards.ts
+++ apps/halprin-web-app/src/features/video/utils/typeguards.ts
@@ -1,8 +1,12 @@
-import { VideoUrl } from '@/data/data.types';
+import { LocalizedVideoUrl, VideoUrl } from '@/data/data.types';
 import { isNonEmptyString, isPlainObject } from '@contredanse/common';
 
 export const isVideoUrl = (v: unknown): v is VideoUrl => {
   if (isNonEmptyString(v)) return true;
+  return isLocalizedVideoUrl(v);
+};
+
+export const isLocalizedVideoUrl = (v: unknown): v is LocalizedVideoUrl => {
   if (!isPlainObject(v)) return false;
   return (
     'fr' in v && 'en' in v && isNonEmptyString(v.fr) && isNonEmptyString(v.en)
