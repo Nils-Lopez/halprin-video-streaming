8e9cbecde2ffc84d0571281f91232a593ee2ef5e
Sébastien Vanvelthem <belgattitude@gmail.com>
2021-09-01 12:36:12 +0200

    feat: add route for tags

---
+++ apps/halprin-web-app/src/pages/tags/[tagSlugs].tsx
@@ -0,0 +1,50 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { BadRequest } from '@tsed/exceptions';
+import { videoConfig } from '@/features/video/video.config';
+import { SupportedLang } from '@/features/video/types';
+import { SiteConfigUtils } from '@/core/config/site- config.utils';
+import { Asserts } from '@contredanse/common';
+
+type Props = {
+  tagSlugs: string[];
+  lang: SupportedLang;
+};
+
+export default function TagsRoute(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  const { lang, tagSlugs } = props;
+  return (
+    <>
+      <h1>Hello here's the tag slugs I received</h1>
+      <pre>{JSON.stringify(tagSlugs)}</pre>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const { locale } = context;
+  if (locale === undefined || !SiteConfigUtils.isSupportedLocale(locale)) {
+    throw new BadRequest('locale is missing or not supported');
+  }
+  const { tagSlugs } = context.params ?? {};
+
+  Asserts.nonEmptyString(tagSlugs, () => {
+    throw new BadRequest('tagSlugs must be a non empty string');
+  });
+
+  const slugs = tagSlugs.split(':').filter((tag) => tag.trim() !== '');
+
+  const { i18nNamespaces } = videoConfig;
+  return {
+    props: {
+      tagSlugs: slugs,
+      lang: locale,
+      // @see https:/github.com/i18next/react-i18next/pull/1340#issuecomment-874728587
+      ...(await serverSideTranslations(locale, i18nNamespaces.slice())),
+    },
+  };
+};
